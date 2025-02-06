8160485592b17b2ae252001b587a5f3c7cf42651
Daniil <dr.chashka@gmail.com>
2025-02-06 03:57:34 +0000

    Сделал милую анимацию.

--- components/card-stack.tsx
+++ components/card-stack.tsx
@@ -1,32 +1,60 @@
 import { Card, CardFooter, CardHeader, Divider } from "@heroui/react";
 import { FC } from "react";
+import { motion } from "framer-motion";
 
 export interface CardStackProps {
     className?: string; 
 }
 
+const cardVariants = {
+    initial: (custom) => ({ scale: custom - 0.5 }),
+    animate: (custom) => ({ scale: custom }),
+};
+
 export const CardStack: FC<CardStackProps> = () => {
     return (
         <div className="relative">
-            <div className={`absolute top-10 scale-${90}`}>
+            <motion.div 
+                className='absolute top-10 scale-90'
+                initial={{ scale: 0 }}
+                animate="animate"
+                custom={0.9}
+                variants={cardVariants}
+                transition={{ duration: 0.5, delay: 0.4 }}
+            >
                 <Card className="w-[400px]" shadow='lg'>
                     <CardHeader className='h-24 justify-center'></CardHeader>
                     <Divider />
                     <CardFooter className='h-24 justify-center'></CardFooter>
                 </Card>
-            </div>
-            <div className={`absolute top-5 scale-${95}`}>
+            </motion.div>
+            <motion.div 
+                className='absolute top-5 scale-95'
+                initial={{ scale: 0 }}
+                animate="animate"
+                custom={0.95}
+                variants={cardVariants}
+                transition={{ duration: 0.5, delay: 0.2 }}
+            >
                 <Card className="w-[400px]" shadow='lg'>
                     <CardHeader className='h-24 justify-center'></CardHeader>
                     <Divider />
                     <CardFooter className='h-24 justify-center'></CardFooter>
                 </Card>
-            </div>
-            <Card className="w-[400px]" shadow='lg'>
-                <CardHeader className='h-24 justify-center'>Forest</CardHeader>
-                <Divider />
-                <CardFooter className='h-24 justify-center'>Лес</CardFooter>
-            </Card>
+            </motion.div>
+            <motion.div 
+                initial="initial"
+                animate="animate"
+                custom={1}
+                variants={cardVariants}
+                transition={{ duration: 0.5 }}
+            >
+                <Card className="w-[400px]" shadow='lg'>
+                    <CardHeader className='h-24 justify-center'>Forest</CardHeader>
+                    <Divider />
+                    <CardFooter className='h-24 justify-center'>Лес</CardFooter>
+                </Card>
+            </motion.div>
         </div>
-    )
+    );
 }
