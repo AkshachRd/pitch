52ce9233a03ac0e88d27fdd258da67d367eb6ab5
Daniil <dr.chashka@gmail.com>
2025-02-07 23:22:27 +0000

    Починил кнопку;.

--- components/cool-button.tsx
+++ components/cool-button.tsx
@@ -2,9 +2,9 @@ import { FC } from 'react';
 
 export const CoolButton: FC = () => {
     return (
-        <div className="relative rounded-lg bg-white p-4 shadow-[0_0_0_1px_rgba(0,0,0,.01)]">
-            <div className="animate-glow -z-10 absolute top-10 left-0 right-0 w-full h-full scale-90 bg-glow-gradient bg-[length:200%_200%] blur-lg filter"></div>
-            Hello
+        <div className="relative rounded-lg p-4 ">
+            <div className="absolute inset-0 left-0 right-0 top-4 h-full w-full scale-90 animate-glow bg-glow-gradient bg-[length:200%_200%] blur-lg"></div>
+            <div className="relative rounded-lg bg-white p-4 shadow-[0_0_0_1px_rgba(0,0,0,.01)]">Hello</div>
         </div>
     );
 };
