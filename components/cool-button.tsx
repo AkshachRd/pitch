f45a68be4e6896686026344c5086ab4cfa2b6a4d
Daniil <dr.chashka@gmail.com>
2025-02-07 21:53:25 +0000

    Поправил стили.

--- components/cool-button.tsx
+++ components/cool-button.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 export const CoolButton: FC = () => {
     return (
         <div className="relative rounded-lg bg-white p-4 shadow-[0_0_0_1px_rgba(0,0,0,.01)]">
-            <div className="animate-glow absolute inset-0 -z-10 scale-90 transform bg-glow-gradient bg-[200%_200%] blur-lg filter"></div>
+            <div className="animate-glow absolute top-10 left-0 right-0 w-full h-full scale-90 bg-glow-gradient bg-[length:200%_200%] blur-lg filter"></div>
             Hello
         </div>
     );
