b1e93f32a713cf8e1f8d3b74329811db9a98055f
Daniil <dr.chashka@gmail.com>
2025-02-07 23:01:55 +0000

    Вернул z-index.

--- components/cool-button.tsx
+++ components/cool-button.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 export const CoolButton: FC = () => {
     return (
         <div className="relative rounded-lg bg-white p-4 shadow-[0_0_0_1px_rgba(0,0,0,.01)]">
-            <div className="animate-glow absolute top-10 left-0 right-0 w-full h-full scale-90 bg-glow-gradient bg-[length:200%_200%] blur-lg filter"></div>
+            <div className="animate-glow -z-10 absolute top-10 left-0 right-0 w-full h-full scale-90 bg-glow-gradient bg-[length:200%_200%] blur-lg filter"></div>
             Hello
         </div>
     );
