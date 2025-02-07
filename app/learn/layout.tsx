42deadb788389a950d1c1f2f1ef4fe2ab6139d64
Daniil <dr.chashka@gmail.com>
2025-02-07 00:19:57 +0000

    Добавл полоски.

--- app/learn/layout.tsx
+++ app/learn/layout.tsx
@@ -1,6 +1,6 @@
 export default function LearnLayout({ children }: { children: React.ReactNode }) {
     return (
-        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 h-full">
             {children}
         </section>
     );
--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -4,6 +4,8 @@ import { useCardStore } from '@/store/store';
 import { CardStack } from '@/components/card-stack';
 import { useState } from 'react';
 import { Card } from '@/types/card';
+import { Button } from '@heroui/react';
+import { Side } from '@/components/side';
 
 export default function LearnPage() {
     // const cards = useCardStore((state) => state.cards);
@@ -14,13 +16,19 @@ export default function LearnPage() {
     ]);
 
     return (
-        <>
+        <div className="flex w-full items-center justify-center h-full">
+            <Side color="red">
+                <Button variant="bordered">No</Button>
+            </Side>
             <CardStack
                 cards={cards}
                 onRemove={() => {
                     setCards((state) => state.slice(1));
                 }}
             />
-        </>
+            <Side color="green">
+                <Button variant="bordered">Yes</Button>
+            </Side>
+        </div>
     );
 }
