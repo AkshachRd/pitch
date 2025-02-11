fccbe5968a91a747cd8923a1d7fb785ab21cd3a2
Daniil Khudyakov <daniil.khudyakov@travelline.ru>
2025-02-11 14:34:26 +0300

    Разработал новый вид стороны.

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,15 +1,14 @@
 'use client';
 
 import { useState } from 'react';
-import { Button } from '@heroui/react';
 
 import { CardStack } from '@/components/card-stack';
-import { Card } from '@/types/card';
+import { Card as CardType } from '@/types/card';
 import { Side } from '@/components/side';
 
 export default function LearnPage() {
     // const cards = useCardStore((state) => state.cards);
-    const [cards, setCards] = useState<Card[]>([
+    const [cards, setCards] = useState<CardType[]>([
         { id: '1', frontSide: 'Forest', backSide: 'Лес' },
         { id: '2', frontSide: 'Conclave', backSide: 'Тайное собрание' },
         { id: '3', frontSide: 'Hope', backSide: 'Надежда' },
@@ -17,18 +16,14 @@ export default function LearnPage() {
 
     return (
         <div className="flex h-full w-full items-center justify-center">
-            <Side color="red">
-                <Button variant="bordered">No</Button>
-            </Side>
+            <Side color="red" />
             <CardStack
                 cards={cards}
                 onRemove={() => {
                     setCards((state) => state.slice(1));
                 }}
             />
-            <Side className="relative rounded-lg bg-white">
-                <Button variant="bordered">Yes</Button>
-            </Side>
+            <Side color="green" />
         </div>
     );
 }
--- components/side.tsx
+++ components/side.tsx
@@ -1,23 +1,35 @@
 'use client';
 
+import { Card } from '@heroui/react';
 import clsx from 'clsx';
-import { FC, ReactNode } from 'react';
+import { FC } from 'react';
 
 interface SideProps {
-    children?: ReactNode;
+    disableAnimation?: boolean;
     color?: string;
     className?: string;
 }
 
-export const Side: FC<SideProps> = ({ children, color, className }: SideProps) => {
+export const Side: FC<SideProps> = ({ color, className, disableAnimation = false }: SideProps) => {
+    const isAnimating = !disableAnimation;
+
     return (
         <div
-            className={clsx(className, 'flex flex-auto items-center justify-center h-full', {
-                'bg-red-300': color === 'red',
-                'bg-green-300': color === 'green',
+            className={clsx(className, 'relative m-6 inline-block h-full flex-auto', {
+                'hover:animate-pulse': isAnimating,
             })}
         >
-            {children}
+            <div
+                className={clsx(
+                    'scale-120 absolute inset-0 left-0 right-0 top-0 h-full w-full bg-[length:200%_200%] blur-lg',
+                    isAnimating && {
+                        'bg-red-600': color === 'red',
+                        'bg-green-600': color === 'green',
+                    },
+                )}
+            />
+
+            <Card isPressable className="h-full w-full" />
         </div>
     );
 };
