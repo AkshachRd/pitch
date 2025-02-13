e3ea2c9b859e2343cf3e427dca005a6f0827a18d
Daniil Khudyakov <daniil.khudyakov@travelline.ru>
2025-02-13 14:56:44 +0300

    Поменял надписи.

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -61,7 +61,7 @@ export default function LearnPage() {
     return (
         <div className="flex h-full w-full items-center justify-center" {...keyboardProps}>
             <Side color="danger" isActive={leftActive}>
-                No
+                Bad
             </Side>
             <div className="flex h-full flex-col justify-evenly">
                 <CardStack
@@ -82,7 +82,7 @@ export default function LearnPage() {
                 />
             </div>
             <Side color="success" isActive={rightActive}>
-                Yes
+                Good
             </Side>
         </div>
     );
