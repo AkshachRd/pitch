759854f30269e5a9f94b435a2f366a87294926a5
Daniil <dr.chashka@gmail.com>
2025-02-22 21:49:05 +0000

    a

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,39 @@
+'use client';
+
+import { Button, Form, Input } from '@heroui/react';
 import { login, signup } from './actions';
 
 export default function LoginPage() {
     return (
-        <form>
-            <label htmlFor="email">Email:</label>
-            <input id="email" name="email" type="email" required />
-            <label htmlFor="password">Password:</label>
-            <input id="password" name="password" type="password" required />
-            <button formAction={login}>Log in</button>
-            <button formAction={signup}>Sign up</button>
-        </form>
+        <div className="flex h-full w-full flex-col items-center justify-center">
+            <Form className="w-full max-w-xs">
+                <Input
+                    isRequired
+                    errorMessage="Please enter a valid email"
+                    label="Email"
+                    labelPlacement="outside"
+                    id="email"
+                    name="email"
+                    placeholder="Enter your email"
+                    type="email"
+                />
+                <Input
+                    isRequired
+                    errorMessage="Please enter a valid password"
+                    label="Password"
+                    labelPlacement="outside"
+                    id="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    type="password"
+                />
+                <Button formAction={login} variant="bordered">
+                    Log in
+                </Button>
+                <Button formAction={signup} variant="bordered">
+                    Sign up
+                </Button>
+            </Form>
+        </div>
     );
 }
