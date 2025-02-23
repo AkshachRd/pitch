8fac3e1de47384745b5180fe9d120176e433a1d7
Daniil <dr.chashka@gmail.com>
2025-02-23 20:37:29 +0000

    Добавил провайдер для реакт квери.

--- app/providers.tsx
+++ app/providers.tsx
@@ -6,6 +6,7 @@ import * as React from 'react';
 import { HeroUIProvider } from '@heroui/system';
 import { useRouter } from 'next/navigation';
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
+import { ReactQueryClientProvider } from '@/components/react-query-client-provider';
 
 export interface ProvidersProps {
     children: React.ReactNode;
@@ -23,7 +24,9 @@ export function Providers({ children, themeProps }: ProvidersProps) {
 
     return (
         <HeroUIProvider navigate={router.push}>
-            <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
+            <NextThemesProvider {...themeProps}>
+                <ReactQueryClientProvider>{children}</ReactQueryClientProvider>
+            </NextThemesProvider>
         </HeroUIProvider>
     );
 }
