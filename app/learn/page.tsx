8266957c90c9226cbb2428f13a9635f4e256a2a0
Daniil Khudyakov <daniil.khudyakov@travelline.ru>
2025-03-28 11:37:40 +0300

    Форматирование

--- app/learn/page.tsx
+++ app/learn/page.tsx
@@ -1,11 +1,11 @@
-import { Learn } from '@/components/learn';
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
-import { cookies } from 'next/headers';
-import { getSupabaseServerClient } from '@/utils/supabase/server'
-import { prefetchQuery } from '@supabase-cache-helpers/postgrest-react-query'
+import { prefetchQuery } from '@supabase-cache-helpers/postgrest-react-query';
+
+import { getSupabaseServerClient } from '@/utils/supabase/server';
+import { Learn } from '@/components/learn';
 import { getCards } from '@/queries/get-cards';
 
-export default async  function LearnPage() {
+export default async function LearnPage() {
     const queryClient = new QueryClient();
     const supabase = await getSupabaseServerClient();
 
