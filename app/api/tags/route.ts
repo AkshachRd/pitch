ff11428cc65e3ecd9916c96a217238a046277168
Daniil <dr.chashka@gmail.com>
2025-04-03 02:51:33 +0300

    Сделал генерацию тегов с помощью deepseek.

---
+++ app/api/tags/route.ts
@@ -0,0 +1,26 @@
+import { streamText } from 'ai';
+import { createOpenRouter } from '@openrouter/ai-sdk-provider';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+const openrouterApiKey = process.env.OPENROUTER_API_KEY;
+
+if (!openrouterApiKey) {
+    throw new Error('OPENROUTER_API_KEY environment variable is not set');
+}
+
+const openrouter = createOpenRouter({ apiKey: openrouterApiKey });
+const model = openrouter('deepseek/deepseek-chat-v3-0324:free');
+
+export async function POST(req: Request) {
+    const { prompt }: { prompt: string } = await req.json();
+
+    const result = streamText({
+        model,
+        prompt: `Generate tags for a card in this context: ${prompt}.
+        Write only generated tags separated by commas, no other text. Use lowercase letters.`,
+    });
+
+    return result.toDataStreamResponse();
+}
--- components/tag-input.tsx
+++ components/tag-input.tsx
@@ -1,26 +1,52 @@
 'use client';
 
-import { Card, CardBody, Input } from '@heroui/react';
+import { Button, Card, CardBody, Input } from '@heroui/react';
+import { useCompletion } from '@ai-sdk/react';
 import { FC } from 'react';
 
 import { Tag } from './tag';
 
 import { Tag as TagType } from '@/types/tag';
 
-interface TagInputProps {
-    tags: TagType[];
-}
+interface TagInputProps {}
 
-export const TagInput: FC<TagInputProps> = ({ tags }: TagInputProps) => {
+export const TagInput: FC<TagInputProps> = ({}: TagInputProps) => {
+    const { completion, input, setInput, handleSubmit } = useCompletion({
+        api: '/api/tags',
+        onFinish: (_, response) => {
+            console.log('response', response);
+        },
+        onError: (error) => {
+            console.error(error);
+        },
+    });
+
     return (
         <Card className="max-w-80">
             <CardBody className="flex-row flex-wrap items-center gap-2">
-                {tags.map((tag) => (
-                    <Tag key={tag.content} color={tag.color}>
-                        {tag.content}
-                    </Tag>
-                ))}
-                <Input className="flex-grow" fullWidth={false} />
+                <form onSubmit={handleSubmit}>
+                    {completion.length > 0 &&
+                        completion.split(',').map((tag, index) => (
+                            <Tag key={index} color={'default'}>
+                                {tag}
+                            </Tag>
+                        ))}
+                    <Button
+                        type="button"
+                        onPress={() => {
+                            setInput(`card_front_side: 'Forest', card_back_side: 'Лес'`);
+                        }}
+                    >
+                        Gen tags
+                    </Button>
+                    <Button type="submit">Submit</Button>
+                    <Input
+                        value={input}
+                        onInput={(e) => setInput(e.currentTarget.value)}
+                        className="flex-grow"
+                        fullWidth={false}
+                    />
+                </form>
             </CardBody>
         </Card>
     );
