b11dbced94fc13dc486c221a40339145f474ba00
Daniil <dr.chashka@gmail.com>
2025-04-06 03:32:02 +0300

    Заюзал правильную либу

--- components/tags-list.tsx
+++ components/tags-list.tsx
@@ -1,31 +1,23 @@
 'use client';
 
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from '@supabase-cache-helpers/postgrest-react-query';
 
 import { Tag } from '@/components/tag';
 import { getTags } from '@/queries/get-tags';
 import { useSupabaseBrowser } from '@/utils/supabase/client';
-import { toTag } from '@/types/tag';
-
+import { TagColor } from '@/types/tag';
 export function TagsList() {
     const supabase = useSupabaseBrowser();
-    const { data: tags = [] } = useQuery({
-        queryKey: ['tags'],
-        queryFn: async () => {
-            const response = await getTags(supabase);
-
-            return response.data?.map(toTag) ?? [];
-        },
-    });
+    const { data: tags = [] } = useQuery(getTags(supabase));
 
     return (
         <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-            {tags.map((tag) => (
+            {tags?.map((tag) => (
                 <div
                     key={tag.name}
                     className="rounded-lg border p-4 transition-colors hover:bg-gray-50"
                 >
-                    <Tag color={tag.color}>{tag.name}</Tag>
+                    <Tag color={tag.color as TagColor}>{tag.name}</Tag>
                 </div>
             ))}
         </div>
