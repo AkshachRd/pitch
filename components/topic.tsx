b1bdc4390b0095f43da8a9ce22c333145bb2e0c8
Daniil <dr.chashka@gmail.com>
2025-04-22 03:31:32 +0300

    Добавил анимцию для блока research

--- components/topic.tsx
+++ components/topic.tsx
@@ -16,26 +16,28 @@ export function Topic() {
     };
 
     return (
-        <section className="flex w-full flex-col rounded-lg border-2 p-4">
-            <h2 className="text-lg">1. Research topics</h2>
-            <Form className="w-full items-center" onSubmit={onSubmit}>
-                <Textarea
-                    errorMessage="Please enter a topic"
-                    label="Research Topics"
-                    labelPlacement="outside"
-                    name="topic"
-                    rows={3}
-                    placeholder="Any topic you want to research"
-                />
-                <Button fullWidth type="submit">
-                    Start thinking
-                </Button>
-                {submitted && (
-                    <div className="text-small text-default-500">
-                        You submitted: <code>{JSON.stringify(submitted)}</code>
-                    </div>
-                )}
-            </Form>
-        </section>
+        <AIAnimationWrapper className="flex w-full flex-col" isLoading={true}>
+            <section className="z-10 flex w-full flex-col rounded-lg border-2 bg-background p-4">
+                <h2 className="text-lg">1. Research topics</h2>
+                <Form className="w-full items-center" onSubmit={onSubmit}>
+                    <Textarea
+                        errorMessage="Please enter a topic"
+                        label="Research Topics"
+                        labelPlacement="outside"
+                        name="topic"
+                        rows={3}
+                        placeholder="Any topic you want to research"
+                    />
+                    <Button fullWidth type="submit">
+                        Start thinking
+                    </Button>
+                    {submitted && (
+                        <div className="text-small text-default-500">
+                            You submitted: <code>{JSON.stringify(submitted)}</code>
+                        </div>
+                    )}
+                </Form>
+            </section>
+        </AIAnimationWrapper>
     );
 }
