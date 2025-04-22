dcc9ebda7d9883908bd00faaf1abb54df4bb596f
Daniil <dr.chashka@gmail.com>
2025-04-22 03:30:39 +0300

    Убрал песполезное свойство

--- components/ai-animation-wrapper.tsx
+++ components/ai-animation-wrapper.tsx
@@ -17,7 +17,7 @@ export const AIAnimationWrapper: FC<AIAnimationWrapperProps> = ({
     return (
         <div className={clsx(className, 'relative inline-block')}>
             <div
-                className={`absolute inset-0 left-0 right-0 top-2 h-full w-full scale-90 bg-glow-gradient bg-[length:200%_200%] blur-lg transition-opacity duration-300 ease-in-out ${isLoading ? 'animate-glow opacity-100' : 'opacity-0'}`}
+                className={`absolute inset-0 left-0 right-0 top-2 h-full w-full bg-glow-gradient bg-[length:200%_200%] blur-lg transition-opacity duration-300 ease-in-out ${isLoading ? 'animate-glow opacity-100' : 'opacity-0'}`}
             />
             {children}
         </div>
