66a79ee33cfb5f755d529bc17d519adf57aa8432
Daniil <dr.chashka@gmail.com>
2025-06-27 00:04:43 +0300

    Удалил хуки

--- queries/create-card.ts
+++
@@ -1,5 +0,0 @@
-import { TypedSupabaseClient } from '@/utils/supabase/types';
-
-export function createCard(client: TypedSupabaseClient) {
-    return client.from('card');
-}
--- queries/create-tag.ts
+++
@@ -1,8 +0,0 @@
-import { TypedSupabaseClient } from '@/utils/supabase/types';
-
-export function createTag(client: TypedSupabaseClient) {
-    return {
-        tag: client.from('tag'),
-        cardHasTag: client.from('card_has_tag'),
-    };
-}
