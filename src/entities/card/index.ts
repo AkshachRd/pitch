cdf4ee390502974921055eb2a93773d29f33d0ad
Daniil Khudyakov <daniil.khudyakov@travelline.ru>
2025-07-03 13:43:41 +0300

    public api

--- src/entities/card/index.ts
+++ src/entities/card/index.ts
@@ -1,3 +1,4 @@
-export * from './model';
-export * from './ui';
-export * from './lib';
+export { CardComponent } from './ui/card-component';
+export { useCardStore } from './model/store';
+export type { CardWithTags } from './model/types';
+export { combineCardsWithTags } from './lib/combine-cards-tags';
